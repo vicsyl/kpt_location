d5c14fac4299de468342e72f647f16a54911baef
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2022-09-22 11:11:47 +0200

    get_dirs_and_keys

--- dataset_utils.py
+++ dataset_utils.py
@@ -3,6 +3,46 @@ import re
 import shutil
 
 
+def get_dirs_and_keys(dirs_to_process, base_dir):
+
+    pattern = re.compile("^scene_cam.*final_preview$")
+    dirs = list_scenes(base_dir)[:dirs_to_process]
+
+    in_dirs = []
+    keys = []
+
+    for dir in dirs:
+        print("processing: {}".format(dir))
+        path = "{}/images".format(dir)
+        if not os.path.isdir(path):
+            continue
+        for prev_dir_name in list(os.listdir(path)):
+            print(prev_dir_name)
+            if pattern.match(prev_dir_name) is not None:
+                full_path = "{}/{}".format(path, prev_dir_name)
+                key = full_path[len(base_dir) + 1:].replace("/", "_")
+                in_dirs.append(full_path)
+                keys.append(key)
+                print(full_path)
+                print(key)
+
+    print("in_dirs: {}".format(in_dirs))
+    print("keys: {}".format(keys))
+    return in_dirs, keys
+
+
+def list_scenes(base_dir):
+    print("Listing scenes in {}".format(base_dir))
+    pattern = re.compile("^ai_0.._0..$")
+    paths = []
+    for cur_dir_name in list(os.listdir(base_dir)):
+        if pattern.match(cur_dir_name) is not None:
+            full_path = "{}/{}".format(base_dir, cur_dir_name)
+            paths.append(full_path)
+
+    return paths
+
+
 def clean_scene(scene_path):
 
     pattern = re.compile("^scene_cam.*final_preview$")
@@ -23,4 +63,7 @@ def clean_scene(scene_path):
 
 
 if __name__ == "__main__":
-    clean_scene("scenes/ai_001_001_foo")
+    #clean_scene("scenes/ai_001_001_foo")
+    # for s in list_scenes("scenes"):
+    #     print(s)
+    dirs, keys = get_dirs_and_keys(1000, "./scenes")
