a0e2c24f60f1f635beba595af7255825c2737769
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2022-10-24 12:19:39 +0200

    SuperPoint rotation

--- rotate_exp.py
+++ rotate_exp.py
@@ -8,18 +8,18 @@ import math
 from PIL import Image
 
 
-def draw_flipped_kpts(img_np, kpts_original, kpts_translated, title):
+def draw_all_kpts(img_np, kpts_original, kpts_transformed, title):
     for kpt in kpts_original:
         kpt.size = 50
         kpt.angle = math.pi / 2
-    for kpt in kpts_translated:
+    for kpt in kpts_transformed:
         kpt.size = 25
         kpt.angle = 0
-    kpts_all_cv = kpts_original + kpts_translated
+    kpts_all_cv = kpts_original + kpts_transformed
     img_kpts = img_np.copy()
     cv.drawKeypoints(img_kpts, kpts_all_cv, img_kpts, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     plt.figure(figsize=(9, 9))
-    title = "{}: {} - translated of size 25, original of size 50".format(title, len(kpts_all_cv))
+    title = "{}: {} - transformed of size 25, original of size 50".format(title, len(kpts_all_cv))
     plt.title(title)
     plt.imshow(img_kpts)
     plt.show()
@@ -27,7 +27,6 @@ def draw_flipped_kpts(img_np, kpts_original, kpts_translated, title):
 
 
 def mnn_generic(pts1, pts2, err_th):
-
     assert len(pts1.shape) == len(pts2.shape)
 
     pts1_r, pts2_r = pts1, pts2
@@ -59,28 +58,35 @@ def mnn_generic(pts1, pts2, err_th):
     return pts1_new, pts2_new, mask1, mask2
 
 
-def translate_experiment(file_path, detector, translation):
+def rotate_experiment(file_path, detector, rotations_90_deg):
+    print(f"experiment for n rotations: {rotations_90_deg}")
 
     img_np_o = np.array(Image.open(file_path))
 
     kpts_0_cv, _ = detector.detect(img_np_o, mask=None)
     kpts_0 = torch.tensor([[kp.pt[1], kp.pt[0]] for kp in kpts_0_cv])
 
-    img_np_t = np.zeros_like(img_np_o)
-    mt = -translation
-    if translation[0] == 0:
-        mt[0] = img_np_o.shape[0]
-    if translation[1] == 0:
-        mt[1] = img_np_o.shape[1]
-    img_np_t[translation[0]:, translation[1]:] = img_np_o[:mt[0], :mt[1]]
+    img_np_r = np.rot90(img_np_o, rotations_90_deg, [0, 1])
 
-    kpts_1_cv, _ = detector.detect(img_np_t, mask=None)
-    kpts_1 = torch.tensor([[kp.pt[1], kp.pt[0]] for kp in kpts_1_cv]) - translation
+    kpts_1_cv, _ = detector.detect(img_np_r, mask=None)
+    kpts_1 = torch.tensor([[kp.pt[1], kp.pt[0]] for kp in kpts_1_cv])
+
+    coord0_max = img_np_r.shape[0] - 1
+    coord1_max = img_np_r.shape[1] - 1
+    for i in range(4 - rotations_90_deg):
+        kpts_10_new = coord1_max - kpts_1[:, 1]
+        kpts_11_new = kpts_1[:, 0].clone()
+        kpts_1[:, 0] = kpts_10_new.clone()
+        kpts_1[:, 1] = kpts_11_new.clone()
+        coord1_max, coord0_max = coord0_max, coord1_max
+
+    for i, e_kpts_1_cv in enumerate(kpts_1_cv):
+        e_kpts_1_cv.pt = (kpts_1[i, 1].item(), kpts_1[i, 0].item())
 
     print("number of kpts: {}, {}".format(kpts_0.shape[0], kpts_1.shape[0]))
 
     # mnn
-    kpts_0_new_1d, kpts_1_new_1d, mask_00, mask_10 = mnn_generic(kpts_0, kpts_1, err_th=2)
+    kpts_0_new_1d, kpts_1_new_1d, mask_00, mask_10 = mnn_generic(kpts_0, kpts_1, err_th=4)
     print("number of filtered kpts: {}, {}".format(kpts_0_new_1d.shape[0], kpts_1_new_1d.shape[0]))
 
     distances = torch.linalg.norm(kpts_1_new_1d - kpts_0_new_1d, axis=1)
@@ -101,27 +107,23 @@ def translate_experiment(file_path, detector, translation):
     kpts_1_cv_matched, kpts_1_cv_unmatched = get_lists(kpts_1_cv, mask_10)
     print("Matched keypoints: {}".format(len(kpts_0_cv_matched) + len(kpts_1_cv_matched)))
     print("Unmatched keypoints: {}".format(len(kpts_0_cv_unmatched) + len(kpts_1_cv_unmatched)))
-    draw_flipped_kpts(img_np_o, kpts_0_cv_matched, kpts_1_cv_matched, "matched keypoints")
-    draw_flipped_kpts(img_np_o, kpts_0_cv_unmatched, kpts_1_cv_unmatched, "unmatched keypoints")
+    draw_all_kpts(img_np_o, kpts_0_cv_matched, kpts_1_cv_matched, "matched keypoints")
+    draw_all_kpts(img_np_o, kpts_0_cv_unmatched, kpts_1_cv_unmatched, "unmatched keypoints")
 
-    mean = (kpts_1_new_1d - kpts_0_new_1d).mean()
-    var = (kpts_1_new_1d - kpts_0_new_1d).var()
+    mean = (kpts_1_new_1d - kpts_0_new_1d).mean(dim=0)
+    var = (kpts_1_new_1d - kpts_0_new_1d).var(dim=0)
     print("mean: {}, variance: {}".format(mean, var))
 
 
-def translate_experiment_loop(detector, img_to_show):
-    for translation in torch.tensor([[4, 4], [4, 0], [0, 4]]):
-        img_dir = "demo_imgs"
-        files = ["{}/{}".format(img_dir, fn) for fn in os.listdir(img_dir)][:img_to_show]
-        for file_path in files:
-            translate_experiment(file_path, detector, translation)
-
-
-def main():
-    from superpoint_local import SuperPointDetector
-    detector = SuperPointDetector()
-    translate_experiment_loop(detector, img_to_show=2)
+def rotate_experiment_loop(detector, img_to_show):
+    img_dir = "demo_imgs"
+    files = ["{}/{}".format(img_dir, fn) for fn in os.listdir(img_dir)][:img_to_show]
+    for file_path in files:
+        for rots in range(1, 4):
+            rotate_experiment(file_path, detector, rots)
 
 
 if __name__ == "__main__":
-    main()
+    from superpoint_local import SuperPointDetector
+    detector = SuperPointDetector()
+    rotate_experiment_loop(detector, img_to_show=1)
