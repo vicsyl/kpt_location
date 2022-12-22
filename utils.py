04e7522fd0f174ae062f8f6e707d807ae8ce3f12
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2022-12-22 15:01:40 +0100

    cuda

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def upscale_double(x):
     # img_t = torch.from_numpy(img).permute(2, 0, 1)[None]
     B, CH, H, W = x.shape
 
-    upscaled = torch.zeros((B, CH, H * 2, W * 2))
+    upscaled = torch.zeros((B, CH, H * 2, W * 2)).to(device=x.device)
     upscaled[:, :, ::2, ::2] = x
     # sh = list(img_t.shape)
     # sh[-2] += 1
